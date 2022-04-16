3302fccbd369835b41bacf9dd10d7e706157227e
Ramonfire <omar.zaida@uir.ac.ma>
2022-04-16 18:14:33 +0000

    tested sql connectionm added i;plementqtion for Detectors

--- src/main/java/Systeme/Main.java
+++ src/main/java/Systeme/Main.java
@@ -1,38 +1,49 @@
 package Systeme;
 
 import Singleton.SingletonJDBC;
+import Users.proxy;
 import Users.users;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.SQLOutput;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Main {
    public static void main(String[] args) {
 
-        int r=0;
-       users test=new users();
-       test.setUsername("test");
-       test.setPassword("test");
-       test.setFirstName("test");
-       test.setLastName("test");
 
-       try{
-           Connection conn = SingletonJDBC.getConnection();
-           String querry = "insert into users values(null, ?, ?, ?, ?)";
-           PreparedStatement pstmt = (PreparedStatement) conn.prepareStatement(querry);
-           pstmt.setString(1, test.getUsername());
-           pstmt.setString(2,test.getPassword() );
-           pstmt.setString(3, test.getFirstName());
-           pstmt.setString(4, test.getLastName());
 
-           r = pstmt.executeUpdate();
-           if (r==1){
-               System.out.println("success");
-           }else System.out.println("failure");
-       }catch (SQLException e){
-           e.printStackTrace();
-       }
+
+       Scanner scS = new Scanner(System.in);
+       proxy p = new proxy();
+       users verify = null;
+
+      // while (true) {
+
+           verify = new users();
+           System.out.println("Insert your email");
+           String email = scS.nextLine();
+           System.out.println("Insert your pasword");
+           String password = scS.nextLine();
+           verify = p.login(email, password);
+           System.out.println(verify.toString());
+
+
+  // }
+
+
+
+
+
+
+
+
+
+
+
    }
     }
 
--- src/main/java/Users/account.java
+++ src/main/java/Users/account.java
@@ -1,20 +1,47 @@
 package Users;
 
+import Singleton.SingletonJDBC;
 import StructureClasses.Visiteur;
 import StructureClasses.activity;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.LinkedList;
 
 
 public abstract class account {
     
     
-    public users login(){
+    public users login(String email,String password){
        users logged =new users();
+
+
+        try{
+            Connection conn = SingletonJDBC.getConnection();
+            String querry = "select * from users where email=? and password=?";
+            PreparedStatement pstmt = (PreparedStatement) conn.prepareStatement(querry);
+            pstmt.setString(1,email);
+            pstmt.setString(2,password );
+
+            ResultSet r = pstmt.executeQuery();
+                if (r.next()){
+                    System.out.println("success");
+                    logged.setUsername((String) r.getObject("email"));
+                    logged.setPassword((String) r.getObject("password"));
+                    logged.setFirstName((String) r.getObject("name"));
+                    logged.setLastName((String) r.getObject("familyname"));
+
+                }else {System.out.println("failure");}
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
         return  logged;
     }
 
-    public void logout(){
+    public users logout(){
+        return new users();
     }
 
     public  void verifyalarms(){
@@ -77,5 +104,8 @@ public abstract class account {
 
         return  response;
     }
+public boolean isLoggedin(users user){
+        return  false;
+}
 
 }
